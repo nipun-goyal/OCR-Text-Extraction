6a10b4759a9cdb656272f23141d16d23ed21f648
nipun-goyal <nipungoyal26@gmail.com>
2022-06-30 09:53:49 -0700

    Organize-codebase

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Extracting name of the entities (environmental consultants) from the cover page of PDF Documents',
+    author='Nipun Goyal',
+    license='MIT',
+)
